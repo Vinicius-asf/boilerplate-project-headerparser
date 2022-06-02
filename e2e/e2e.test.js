f3c9ff39297bb11c1e559302bc2ef2f221e5b6e7
Vinicius Ferreira <viniciusant3@gmail.com>
2022-06-02 17:23:28 -0300

    fix: test with set headers

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,3 +1,4 @@
+const { agent } = require('supertest')
 const request = require('supertest')
 const createServer = require('../server')
 
@@ -7,6 +8,11 @@ describe('Endpoints', () => {
   it('receive valid JSON', async () => {
     const res = await request(app)
       .get('/api/whoami')
+      .set({
+        'x-forward-for':'192.168.29.29',
+        'accept-language':'"pt-BR,pt;q=0.9',
+        'user-agent':'Mozilla/5.0'
+    })
     expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveProperty("ipaddress")
     expect(res.body).toHaveProperty("language")
